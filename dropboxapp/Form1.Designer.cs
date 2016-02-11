4ff6420e2ece8d20fdd04ccfad4db30435cfaa8a
vaibhav chaudhari <work.vaibhav26@gmail.com>
2016-02-11 11:49:24 +0530

    mygoogledriveapp

---
+++ dropboxapp/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace dropboxapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "create folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(161, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "upload";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(71, 88);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 293);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ dropboxapp/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Nemiro.OAuth;
+using Nemiro.OAuth.LoginForms;
+using System.IO;
+
+namespace dropboxapp
+{
+    public partial class Form1 : Form
+    {
+        private string CurrentPath = "/";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Properties.Settings.Default.AccessToken))
+            {
+                this.GetAccessToken();
+            }
+            else
+            {
+                this.GetFiles();
+            }
+
+        }
+        private void GetAccessToken()
+        {
+            var login = new DropboxLogin("jdp496lzj6t5xk0", "p7n662eh3gvxbwq");
+            login.Owner = this;
+            login.ShowDialog();
+            if (login.IsSuccessfully)
+            {
+                Properties.Settings.Default.AccessToken = login.AccessToken.Value;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                MessageBox.Show("error while login");
+            }
+        }
+        private void GetFiles()
+        {
+            OAuthUtility.GetAsync
+                (
+                "http://api.dropbox.com/1/metadata/auto/",
+                new HttpParameterCollection
+                {
+                    {"path", this.CurrentPath },
+                    {"access_token",Properties.Settings.Default.AccessToken }
+                },
+                callback: GetFiles_Result
+                );
+        }
+
+        private void GetFiles_Result(RequestResult result)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action<RequestResult>(GetFiles_Result), result);
+                return;
+            }
+            if (result.StatusCode == 200)
+            {
+                checkedListBox1.Items.Clear();
+
+                foreach (UniValue file in result["contents"])
+                {
+                    this.Invoke((MethodInvoker)(() => checkedListBox1.Items.Add(file["path"])));
+                   //listBox1.Items.Add(file["path"]);
+                }
+                
+                if (this.CurrentPath != "/")
+                {
+                    checkedListBox1.Items.Insert(0, "...");
+                }
+            }
+            else
+            {
+                MessageBox.Show("error");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OAuthUtility.PostAsync
+                (
+                "https://api.dropbox.com/1/fileops/create_folder",
+                new HttpParameterCollection
+                {
+                    {"access_token", Properties.Settings.Default.AccessToken },
+                    {"root","auto" },
+                    {"path", Path.Combine(this.CurrentPath, textBox1.Text).Replace("\\","/") }
+                },
+                callback: create_folder_result
+                );
+        }
+        private void create_folder_result(RequestResult result)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action<RequestResult>(create_folder_result), result);
+                return;
+            }
+            if (result.StatusCode == 200)
+            {
+                this.GetFiles();
+            }
+            else
+            {
+                if (result["erros"].HasValue)
+                {
+                    MessageBox.Show(result["erros"].ToString());
+                }
+                else
+                {
+                    MessageBox.Show(result.ToString());
+                }
+            }
+
+
+        }
+
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (checkedListBox1.SelectedItem.ToString() == "")
+            {
+                if (this.CurrentPath != "/")
+                {
+                    this.CurrentPath = Path.GetDirectoryName(this.CurrentPath).Replace("\\", "/");
+                }
+            }
+            else
+            {
+                this.CurrentPath = checkedListBox1.SelectedItem.ToString();
+            }
+            this.GetFiles();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != System.Windows.Forms.DialogResult.OK) { return; }
+            string path = Path.Combine(this.CurrentPath, Path.GetFileName(openFileDialog1.FileName)).Replace("\\", "/");
+            OAuthUtility.PutAsync
+                (
+                "https://api-content.dropbox.com/1/files_put/auto/",
+                new HttpParameterCollection
+                {
+                    {"access_token", Properties.Settings.Default.AccessToken },
+                    {"path",Path.Combine(this.CurrentPath,Path.GetFileName(openFileDialog1.FileName)).Replace("\\","/") },
+                    {"overwrite","true" },
+                    {"autoname","true" },
+                    { openFileDialog1.OpenFile() }
+                },
+                callback: upload_file
+                );
+    }
+
+        private void upload_file(RequestResult result)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action<RequestResult>(upload_file), result);
+                return;
+            }
+            if (result.StatusCode == 200)
+            {
+                this.GetFiles();
+            }
+            else
+            {
+                if (result["erros"].HasValue)
+                {
+                    MessageBox.Show(result["erros"].ToString());
+                }
+                else
+                {
+                    MessageBox.Show(result.ToString());
+                }
+            }
+
+
+        }
+    }
+}
